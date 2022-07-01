469044108e1071b8c6905688582fca6b1ff5aac3
Dmitriy Pereguda <asmoday.p@gmail.com>
2022-07-01 05:57:12 +0300

    235

---
+++ 235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        node = root
+        while node:
+            if node.val < p.val and node.val < q.val:
+                node = node.right
+            elif node.val > p.val and node.val > q.val:
+                node = node.left
+            else:
+                break
+        return node
